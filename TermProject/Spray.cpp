d32c38e9777563fcc523b633e2a3dd8292b321e0
권용민 <briandr@naver.com>
2022-05-25 21:42:44 +0900

    스프레이 미사일 move 함수 구현

--- TermProject/Spray.cpp
+++ TermProject/Spray.cpp
@@ -1,10 +1,9 @@
 #include "Spray.h"
 
 Spray::Spray(int x, int y) : gold(300), w(100), h(100), delay(66) {
-	SDL_Surface* tylenol_surface = IMG_Load("../../Resources/turret_03_spray.png");
-	//SDL_SetColorKey(tylenol_surface, SDL_TRUE, SDL_MapRGB(tylenol_surface->format, 255, 255, 255));
-	spray_texture = SDL_CreateTextureFromSurface(g_renderer, tylenol_surface);
-	SDL_FreeSurface(tylenol_surface);
+	SDL_Surface* spray_surface = IMG_Load("../../Resources/turret_03_spray.png");
+	spray_texture = SDL_CreateTextureFromSurface(g_renderer, spray_surface);
+	SDL_FreeSurface(spray_surface);
 	spray_source = { 0,0,100,100 };
 	spray_destination = { x - 50, y - 50, spray_source.w, spray_source.h };
 
@@ -40,13 +39,13 @@ void Spray::shooting() {
 
 void Spray::missileMove() {
 	for (auto iter = missile_top.begin(); iter != missile_top.end(); iter++) {
-		(*iter)->move();
+		(*iter)->move(0, spray_destination.x, spray_destination.y);
 	}
 	for (auto iter = missile_middle.begin(); iter != missile_middle.end(); iter++) {
-		(*iter)->move();
+		(*iter)->move(1, spray_destination.x, spray_destination.y);
 	}
 	for (auto iter = missile_bottom.begin(); iter != missile_bottom.end(); iter++) {
-		(*iter)->move();
+		(*iter)->move(2, spray_destination.x, spray_destination.y);
 	}
 }
 
